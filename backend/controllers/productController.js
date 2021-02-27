840338f26b50d2cb1db864fb4e4b4673bef6e9f8
Md. Sadiqul Islam <sadiqul8421@outlook.com>
2021-02-27 17:57:39 +0600

    product crud added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,17 +4,74 @@ const Product = require('../models/product');
 
     const product = await Product.create(req.body);
 
-     res.status(201).json({
+     return res.status(201).json({
         success: true,
         product
-     })
+     });
 }
 
 
-exports.getProducts = (req,res,next)=>{
-     console.log(req);
-     res.status(200).json({
+exports.getProducts = async (req,res,next)=>{
+     const products = await Product.find();
+    return res.status(200).json({
          success:true,
-         message:'Fine'
-     })
+         count: products.length,
+         products
+     });
 }
+exports.getProduct = async (req,res,next)=>{
+     console.log(req)
+    const product = await Product.findById(req.params.id);
+    if(!product){
+        return  res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        });
+    }
+    return   res.status(200).json({
+        success:true,
+        product
+    });
+
+}
+exports.updateProduct = async (req,res,next)=>{
+     console.log(req.body)
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new : true,
+        runValidators: true,
+        useFindAndModify: true
+    });
+    if(!product){
+        return  res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        });
+    }
+    return  res.status(200).json({
+        success:true,
+
+    });
+
+}
+
+
+exports.deleterProduct = async (req,res,next)=>{
+    console.log(req.body)
+    const product = await Product.findById(req.params.id);
+    if(!product){
+      return   res.status(404).json({
+            success:false,
+            message: 'Product not found'
+        });
+    }
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        message:'Product deleted'
+
+    });
+
+}
+
+
+
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const{ getProducts,newProduct} = require('../controllers/productController');
+const{ getProducts,newProduct,getProduct,updateProduct, deleterProduct} = require('../controllers/productController');
 router.route('/products').get(getProducts);
-router.route('/product/new').post(newProduct);
+router.route('/admin/product/new').post(newProduct);
+router.route('/admin/product/:id').get(getProduct);
+router.route('/admin/product/:id').put(updateProduct)
+    .delete(deleterProduct)
+;
 module.exports = router;
